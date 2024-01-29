c4b34fb0bd39b3bb03c685c7cb6f171c9b42f658
demoavi <157693200+demoavi@users.noreply.github.com>
2024-01-29 12:33:07 +0100

    Update avi_objects.tf

--- avi_objects.tf
+++ avi_objects.tf
@@ -1,14 +1,1 @@
-resource "avi_healthmonitor" "hm" {
-  count = var.avi_count
-  name = "${var.avi_healthmonitor.basename}${count.index + 1 }"
-  tenant_ref = data.avi_tenant.tenant[count.index].id
-  type = var.avi_healthmonitor.type
-  receive_timeout = var.avi_healthmonitor.receive_timeout
-  failed_checks = var.avi_healthmonitor.failed_checks
-  send_interval = var.avi_healthmonitor.send_interval
-  successful_checks = var.avi_healthmonitor.successful_checks
-  http_monitor {
-    http_request = var.avi_healthmonitor.http_request
-    http_response_code = var.http_response_code
-  }
-}
+
