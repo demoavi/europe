287949d14399ba62dd7f711205f07a8869129bbe
demoavi <157693200+demoavi@users.noreply.github.com>
2024-01-29 12:59:46 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,3 @@
 output "hello_world" {
   value = "hello Avi Demo"
 }
-
-resource "null_resource" "test_directory" {
-  provisioner "local-exec" {
-    command = "pwd | tee /home/ubuntu/tf.txt"
-  }
-}
