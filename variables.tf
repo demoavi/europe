c858996fea6bb23f2e65a5274454f870aff2da97
demoavi <157693200+demoavi@users.noreply.github.com>
2024-01-29 11:42:24 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,17 @@ variable "avi_tenant" {
 variable "avi_cloud" {
   default = "Default-Cloud"
 }
+
+variable "healthmonitor" {
+  type = map
+  default = {
+    basename = "github-hm-"
+    count = 1
+    type = "HEALTH_MONITOR_HTTP"
+    receive_timeout = "1"
+    failed_checks = "2"
+    send_interval= "1"
+    successful_checks = "2"
+    http_request= "HEAD / HTTP/1.0"
+  }
+}
