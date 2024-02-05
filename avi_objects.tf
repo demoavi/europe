52f606c6f354ae7976b1affc8b42674f96c3bf3c
demoavi <157693200+demoavi@users.noreply.github.com>
2024-02-05 10:12:44 +0100

    Update avi_objects.tf

--- avi_objects.tf
+++ avi_objects.tf
@@ -12,3 +12,26 @@ resource "avi_healthmonitor" "hm" {
     http_response_code = var.http_response_code
   }
 }
+
+resource "avi_pool" "pool" {
+  count = var.avi_count
+  name = "${var.avi_pool.basename}${count.index + 1 }"
+  tenant_ref = data.avi_tenant.tenant.id
+  lb_algorithm = var.pool.lb_algorithm
+  cloud_ref = data.avi_cloud.default_cloud.id
+  health_monitor_refs = ["${data.avi_healthmonitor.hm[count.index].id}"]
+  dynamic servers {
+    for_each = [for server in var.avi_servers:{
+      addr = server.ip
+      type = server.type
+      port = server.port
+    }]
+    content {
+      ip {
+        type = servers.value.type
+        addr = servers.value.addr
+      }
+      port = servers.value.port
+    }
+  }
+}
