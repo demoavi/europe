b678b75c4c27d1b360cd1cdc0a890a5f27dbc84a
demoavi <157693200+demoavi@users.noreply.github.com>
2024-02-05 12:53:15 +0100

    Update avi_objects.tf

--- avi_objects.tf
+++ avi_objects.tf
@@ -12,7 +12,7 @@ resource "avi_tenant" "tenant" {
 resource "avi_healthmonitor" "hm" {
   count = var.avi_create == true ? length(var.attendees_list) : 0
   name = "${var.avi_tenant.basename}${count.index + 1 }-${var.avi_healthmonitor.basename}"
-  tenant_ref = data.avi_tenant.tenant[count.index].id
+  tenant_ref = "/api/tenant?name=${var.avi_tenant.basename}${count.index + 1 }"
   type = var.avi_healthmonitor.type
   receive_timeout = var.avi_healthmonitor.receive_timeout
   failed_checks = var.avi_healthmonitor.failed_checks
@@ -27,7 +27,7 @@ resource "avi_healthmonitor" "hm" {
 resource "avi_pool" "pool" {
   count = var.avi_create == true ? length(var.attendees_list) : 0
   name = "${var.avi_tenant.basename}${count.index + 1 }-${var.avi_pool.basename}"
-  tenant_ref = data.avi_tenant.tenant[count.index].id
+  tenant_ref = "/api/tenant?name=${var.avi_tenant.basename}${count.index + 1 }"
   lb_algorithm = var.avi_pool.lb_algorithm
   cloud_ref = data.avi_cloud.default_cloud.id
   health_monitor_refs = ["${avi_healthmonitor.hm[count.index].id}"]
@@ -50,7 +50,7 @@ resource "avi_pool" "pool" {
 resource "avi_vsvip" "vsvip" {
   count = var.avi_create == true ? length(var.attendees_list) : 0
   name = "${var.avi_tenant.basename}${count.index + 1 }-${var.avi_vsvip.basename}"
-  tenant_ref = data.avi_tenant.tenant[count.index].id
+  tenant_ref = "/api/tenant?name=${var.avi_tenant.basename}${count.index + 1 }"
   cloud_ref = data.avi_cloud.default_cloud.id
   vip {
     vip_id = 1
@@ -78,7 +78,7 @@ resource "avi_virtualservice" "https_vs" {
   name = "${var.avi_tenant.basename}${count.index + 1 }-${var.avi_virtualservice.basename}"
   pool_ref = avi_pool.pool[count.index].id
   cloud_ref = data.avi_cloud.default_cloud.id
-  tenant_ref = data.avi_tenant.tenant[count.index].id
+  tenant_ref = "/api/tenant?name=${var.avi_tenant.basename}${count.index + 1 }"
   ssl_key_and_certificate_refs = [data.avi_sslkeyandcertificate.ssl_cert.id]
   ssl_profile_ref = data.avi_sslprofile.ssl_profile.id
   application_profile_ref = data.avi_applicationprofile.application_profile.id
