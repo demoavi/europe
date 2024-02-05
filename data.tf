f7051659a3c145a1b24bd67c64208b6295037088
demoavi <157693200+demoavi@users.noreply.github.com>
2024-02-05 09:59:43 +0100

    Update data.tf

--- data.tf
+++ data.tf
@@ -2,3 +2,9 @@ data "avi_tenant" "tenant" {
   count = var.avi_count
   name = var.avi_tenant
 }
+
+data "avi_healthmonitor" "hm" {
+  count = var.avi_count
+  depends_on = [avi_healthmonitor.hm]
+  name = "${var.avi_healthmonitor.basename}${count.index + 1 }"
+}
