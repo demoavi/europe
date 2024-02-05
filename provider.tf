e26d19192470a2d77896e881eac4aa454bef2338
demoavi <157693200+demoavi@users.noreply.github.com>
2024-02-05 13:19:47 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,6 @@ provider "avi" {
   avi_username   = var.avi_credentials.username
   avi_password   = var.avi_credentials.password
   avi_controller = var.avi_credentials.controller
-  avi_tenant     = var.avi_tenant.default
   avi_version    = var.avi_credentials.api_version
 }
 
