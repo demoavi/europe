8b43bd5111c761899d9a242aade5e03448141d96
demoavi <157693200+demoavi@users.noreply.github.com>
2024-02-05 11:09:43 +0100

    Update data.tf

--- data.tf
+++ data.tf
@@ -12,3 +12,19 @@ data "avi_healthmonitor" "hm" {
 data "avi_cloud" "default_cloud" {
   name = var.avi_cloud
 }
+
+data "avi_sslkeyandcertificate" "ssl_cert" {
+  name = var.avi_virtualservice.sslCert
+}
+
+data "avi_sslprofile" "ssl_profile" {
+  name = var.avi_virtualservice.sslProfile
+}
+
+data "avi_applicationprofile" "application_profile" {
+  name = var.avi_virtualservice.applicationProfile
+}
+
+data "avi_networkprofile" "network_profile" {
+  name = var.avi_virtualservice.networkProfile
+}
